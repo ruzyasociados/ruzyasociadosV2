66716b73970bbfaedd50872e1386947b64a98d70
Administrador <administrador@C4.local>
2024-10-16 10:03:54 -0600

    Arreglo en la barra de navegación

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,15 @@
 let prevScrollpos = window.pageYOffset;
 const navbar = document.getElementById("menu");
-const threshold = 50; // Ajusta este valor según tu necesidad
 
 window.onscroll = function() {
   let currentScrollPos = window.pageYOffset;
+  const windowHeight = window.innerHeight;
 
-  if (currentScrollPos <= threshold) {
-    navbar.style.top = "0"; // Muestra la barra si está cerca de la parte superior
+  // La mitad de la altura de la ventana
+  const halfWindowHeight = windowHeight / 2;
+
+  if (currentScrollPos <= halfWindowHeight) {
+    navbar.style.top = "0"; // Muestra la barra si está en la parte superior
   } else if (prevScrollpos > currentScrollPos) {
     navbar.style.top = "0"; // Muestra la barra al subir
   } else {
