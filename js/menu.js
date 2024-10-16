bbe93d8154cd5cbaf5a7e35e75244648c47cb0a7
Administrador <administrador@C4.local>
2024-10-16 09:59:26 -0600

    Arreglo en la barra de navegación

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,17 @@
 let prevScrollpos = window.pageYOffset;
 const navbar = document.getElementById("menu");
+const threshold = 50; // Ajusta este valor según tu necesidad
 
 window.onscroll = function() {
   let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    navbar.style.top = "0";
+
+  if (currentScrollPos <= threshold) {
+    navbar.style.top = "0"; // Muestra la barra si está cerca de la parte superior
+  } else if (prevScrollpos > currentScrollPos) {
+    navbar.style.top = "0"; // Muestra la barra al subir
   } else {
-    navbar.style.top = "-80px"; // Ajusta este valor según la altura de tu navbar
+    navbar.style.top = "-80px"; // Oculta la barra al bajar
   }
+
   prevScrollpos = currentScrollPos;
-}
+};
